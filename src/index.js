b433aa3733d9eaf1ccea6aaa78e2fe5c0ad34d53
Daryll Wong <daryll_wong@hotmail.com>
2020-03-01 15:04:33 +0800

    add browser router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 //import React, {Component} from 'react'
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import Main from './Components/Main.jsx'
 import './styles/stylesheet.css'
+import {BrowserRouter} from 'react-router-dom'
 
 
 
 
-
-
-ReactDOM.render(<Main/>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'))
